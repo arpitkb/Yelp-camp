09b4de1283f03e30532916e732a9cddf312092a6
arpitkb <bansal_851937@student.nitw.ac.in>
2021-08-31 19:01:29 +0530

    set PORT

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err })
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`serving on port ${port}`)
 })
